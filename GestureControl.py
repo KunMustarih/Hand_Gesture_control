55700c081670c9580a5b5f4e92c51416da951fcb
Mohammed Abdikebir <meloomak@gmail.com>
2022-05-30 22:10:27 -0400

    Made a function of volume control

--- GestureControl.py
+++ GestureControl.py
@@ -14,19 +14,23 @@ maxVolume = 100
 time.sleep(10)
 
 WCAM, HCAM = 640, 480
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 media_player.play()
 cap.set(3, WCAM)
 cap.set(4, HCAM)
 
 detector = htm.handDetector(detectionCon=0.7)
 
+def control_volume (length):
+    volume = np.interp(length, [20, 145], [minVolume, maxVolume])
+    media_player.audio_set_volume(int(volume))
+
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmlist = detector.findPosition(img, draw=False)
     if len(lmlist) != 0:
-        # 145 and 10
 
         x1, y1 = lmlist[4][1], lmlist[4][2]
         x2, y2 = lmlist[8][1], lmlist[8][2]
@@ -39,12 +43,9 @@ while True:
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
         length = math.hypot(x2 - x1, y2 - y1)
-        if length < 20:
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
-
-        volume = np.interp(length, [20, 145], [minVolume, maxVolume])
-        media_player.audio_set_volume(int(volume))
+        control_volume(length)
 
     cv2.imshow("Video", img)
 
     cv2.waitKey(1)
+
