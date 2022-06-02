192bbdd0387cad345241445a41d8db56dc7e3064
Mohammed Abdikebir <meloomak@gmail.com>
2022-06-01 22:06:29 -0400

    Added forward and backward functions. All functions work as required

--- GestureControl.py
+++ GestureControl.py
@@ -2,7 +2,6 @@ import cv2
 import HandTrackingModule as htm
 import pyautogui
 
-
 WCAM, HCAM = 640, 480
 state = "play"
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -14,18 +13,29 @@ detector = htm.handDetector(detectionCon=0.7)
 pyautogui.getWindowsWithTitle("vlc")[0].activate()
 pyautogui.press('space')
 
+
 def pause_video():
     pyautogui.press('space')
+
 
 def play_video():
     pyautogui.press('space')
 
+
 def Volume_Up():
     pyautogui.press('volumeup')
+
 
 def Volume_down():
     pyautogui.press('volumedown')
 
+
+def move_backward():
+    pyautogui.press('left')
+
+
+def move_forward():
+    pyautogui.press('right')
 
 
 while True:
@@ -44,12 +54,17 @@ while True:
             play_video()
             state = "play"
 
-        if a == [0,1,1,1,0]:
+        if a == [0, 1, 1, 1, 0]:
             Volume_down()
 
-        if a == [0,1,1,1,1]:
+        if a == [0, 1, 1, 1, 1]:
             Volume_Up()
 
+        if a == [0, 1, 0, 0, 0]:
+            move_backward()
+
+        if a == [0, 1, 1, 0, 0]:
+            move_forward()
 
     cv2.imshow("Video", img)
     cv2.waitKey(1)
